a556f5937cb71d6c0717bb86250069f20cf55b20
Sean Ferguson <seanferguson8148@gmail.com>
2024-02-04 00:20:08 -0500

    basic lua configuration added

--- init.lua
+++ init.lua
@@ -23,6 +23,16 @@ vim.opt.splitbelow = true
 
 vim.g.netrw_banner = 0
 
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+-- TODO i think i would like to highlight then turn it off
+-- vim.opt.hlsearch = false
+
+vim.opt.laststatus = 3
+
+-- still not sure this does anything
+vim.opt.termguicolors = true
+
 -- copy handled by kitty "control-c"
 -- "control-v" to paste
 vim.opt.clipboard = 'unnamedplus'
@@ -32,24 +42,27 @@ vim.opt.mousefocus = true
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
--- if vim.bo.filetype == 'lua' or vim.bo.filetype == 'javascript' then
---   vim.opt.shiftwidth = 2
---   vim.opt.tabstop = 2
--- end
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ',' -- what is this?
 
-local opts = {noremap = true}
+-- SF i forget about this
+local opts = { noremap = true }
 
 vim.keymap.set('n', ';', ':', opts)
 vim.keymap.set('v', ';', ':', opts)
 
 -- move lines, this is pretty nice
-vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
-vim.keymap.set('v', 'K', ':m \'>-2<CR>gv=gv')
+-- TODO get this desc thing down for whichkey
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'move line down' })
+vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
 
-vim.keymap.set('n', '<leader>q', '<cmd>q!<cr>')
+vim.keymap.set('n', '<leader>q', '<cmd>q!<cr>', { desc = 'quit without saving' })
+vim.keymap.set('n', '<leader>x', '<cmd>update<cr><cmd>q<cr>')
+vim.keymap.set('n', '<leader>m', '<cmd>!make<cr>')
+
+vim.keymap.set('n', 'Q', 'q', opts)
+vim.keymap.set('n', 'q', '<Nop>')
 
 -- whats going on here?
 -- local autocmd = vim.api.nvim_create_autocmd
@@ -59,9 +72,12 @@ vim.cmd([[au TextYankPost * silent! lua vim.highlight.on_yank()]])
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
-        'git', 'clone', '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git', '--branch=stable', -- latest stable release
-        lazypath
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
@@ -75,42 +91,45 @@ require('lazy').setup({
         config = function()
             -- load the colorscheme here
             vim.cmd([[colorscheme tokyonight-night]])
-        end
-    }, {
+        end,
+    },
+    {
         'nvim-lualine/lualine.nvim',
-        dependencies = {'nvim-tree/nvim-web-devicons'},
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
         opts = {
             options = {
                 theme = 'auto',
-                component_separators = {left = '', right = ''},
-                section_separators = {left = '', right = ''}
-            }
-        }
-    }, {
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+            },
+        },
+    },
+    {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         config = function()
-            require('nvim-treesitter.configs').setup {
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = 'all',
+                sync_install = true,
+                auto_install = true,
+                ignore_install = {},
                 highlight = {
                     enable = true,
-                    additional_vim_regex_highlighting = false
-                }
-            }
-        end
-        -- opts = {
-        --     highlight = {
-        --         enable = true,
-        --         additional_vim_regex_highlighting = false
-        --     }
-        -- }
-    }, {
+                    additional_vim_regex_highlighting = false,
+                },
+                modules = {}, -- SF not too sure about this one, fixes linting error
+            })
+        end,
+    },
+    {
         'numToStr/Comment.nvim',
         opts = {
             -- add any options here
         },
-        lazy = false
+        lazy = false,
         -- config = function() require('Comment').setup() end
-    }, {
+    },
+    {
         'folke/which-key.nvim',
         event = 'VeryLazy',
         init = function()
@@ -121,50 +140,85 @@ require('lazy').setup({
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
-        }
+        },
         -- config = function() require('which-key').setup() end
-    }, {
+    },
+    {
         'windwp/nvim-autopairs',
         event = 'InsertEnter',
-        opts = {} -- this is equalent to setup({}) function
-    }, {
+        opts = {}, -- this is equalent to setup({}) function
+    },
+    {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.5',
         -- lazy = true,
-        dependencies = {'nvim-lua/plenary.nvim'},
+        dependencies = { 'nvim-lua/plenary.nvim' },
         cmd = 'Telescope',
-        keys = {{'<leader>ff', '<cmd>Telescope find_files<cr>'}}
-    }, {
+        keys = { { '<leader>ff', '<cmd>Telescope find_files<cr>' } },
+    },
+    {
         'neovim/nvim-lspconfig',
         dependencies = {
-            'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim',
-            'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline',
-            'hrsh7th/nvim-cmp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip'
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/nvim-cmp',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'mfussenegger/nvim-lint',
+            'WhoIsSethDaniel/mason-tool-installer.nvim',
+            'folke/neodev.nvim',
         },
         config = function()
-            require('mason').setup()
+            local mason = require('mason')
+            local mason_tool_installer = require('mason-tool-installer')
+
+            mason.setup({
+                ui = {
+                    icons = {
+                        package_installed = '✓',
+                        package_pending = '➜',
+                        package_uninstalled = '✗',
+                    },
+                },
+            })
+
+            mason_tool_installer.setup({
+                ensure_installed = { 'luacheck', 'stylua' },
+            })
+
+            require('neodev').setup({})
+
             require('mason-lspconfig').setup({
-                ensure_installed = {'lua_ls'},
+                ensure_installed = { 'lua_ls' },
                 handlers = {
                     function(server_name)
                         print('setting up ', server_name)
-                        require('lspconfig')[server_name].setup {}
-                    end
-                }
+                        require('lspconfig')['lua_ls'].setup({
+                            -- settings = {
+                            --     Lua = {
+                            --         diagnostics = { globals = { 'vim' } },
+                            --         runtime = {
+                            --             -- version = 'LuaJIT',
+                            --         },
+                            --     },
+                            -- },
+                        })
+                    end,
+                },
             })
 
-            local cmp = require 'cmp'
+            local cmp = require('cmp')
 
             cmp.setup({
                 snippet = {
                     -- REQUIRED - you must specify a snippet engine
                     expand = function(args)
-                        -- vim.fn['vsnip#anonymous'](args.body) -- For `vsnip` users.
                         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                    end
+                    end,
                 },
                 window = {
                     -- completion = cmp.config.window.bordered(),
@@ -175,12 +229,66 @@ require('lazy').setup({
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm({select = true}) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 }),
                 sources = cmp.config.sources({
-                    {name = 'nvim_lsp'}, {name = 'luasnip'} -- For luasnip users.
-                }, {{name = 'buffer'}})
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                }, { { name = 'buffer' } }),
             })
-        end
-    }
+        end,
+    },
+    {
+        'mfussenegger/nvim-lint',
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = function()
+            local lint = require('lint')
+
+            lint.linters_by_ft = { lua = { 'luacheck' } }
+
+            local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+
+            vim.api.nvim_create_autocmd({
+                'BufEnter',
+                'BufWritePost',
+                'InsertLeave',
+            }, {
+                group = lint_augroup,
+                callback = function()
+                    lint.try_lint()
+                end,
+            })
+
+            vim.keymap.set('n', '<leader>l', function()
+                lint.try_lint()
+            end, { desc = 'Trigger linting for current file' })
+        end,
+    },
+    {
+        'stevearc/conform.nvim',
+        event = { 'BufReadPre', 'BufNewFile' },
+        config = function()
+            local conform = require('conform')
+
+            conform.setup({
+                formatters_by_ft = {
+                    lua = { 'stylua' },
+                    -- python = { "isort", "black" },
+                },
+                format_on_save = {
+                    lsp_fallback = true,
+                    async = false,
+                    timeout_ms = 500,
+                },
+            })
+
+            vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
+                conform.format({
+                    lsp_fallback = true,
+                    async = false,
+                    -- timeout_ms = 500
+                })
+            end, { desc = 'Format file or range (in visual mode)' })
+        end,
+    },
 })
